3909d132a20afed967a93805072b92b1bf051a15
Carlos Paiva <paivakk22@gmail.com>
2024-05-16 23:32:01 -0300

    added db to route delete

--- routers/deleteRegister.py
+++ routers/deleteRegister.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from services.deleteRegisterService import delete_register_by_id
 
 router = APIRouter()
 
 
 @router.delete("/delete-register/{register_id}", status_code=200)
-async def delete_register_id():
-    return {"message": "Sorry. This API is building."}
+async def delete_register(register_id: int):
+    result = await delete_register_by_id(register_id)
+    if not result["success"]:
+        raise HTTPException(status_code=404, detail=result["message"])
+    return {"message": result["message"]}
