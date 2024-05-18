c1e43c1fc8f68a60af275aba4bdade1e2b955c79
Carlos Paiva <paivakk22@gmail.com>
2024-05-17 21:59:46 -0300

    create createRegisterQuery and work in the route

--- services/createRegisterService.py
+++ services/createRegisterService.py
@@ -1,5 +1,6 @@
 import re
 from database.connection import connect_to_postgres
+from database.register.createRegisterQuery import create_register_query
 
 
 async def create_register(new_register):
@@ -9,12 +10,7 @@ async def create_register(new_register):
 
     try:
         # Executa a consulta SQL para inserir os dados na tabela
-        await conn.execute("INSERT INTO register (name, cpf, address, phone) "
-                           "VALUES ($1, $2, $3, $4)",
-                           new_register.name,
-                           new_register.cpf,
-                           new_register.address,
-                           new_register.phone)
+        await create_register_query(conn, new_register)
     finally:
         # Fecha a conexão com o banco de dados
         await conn.close()
