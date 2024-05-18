fa33fec2634e0e0fa13243e9323e44b08c550be0
Carlos Paiva <paivakk22@gmail.com>
2024-05-18 19:03:10 -0300

    added automation test

--- tests/api/test_create_register.py
+++ tests/api/test_create_register.py
@@ -1,16 +1,17 @@
-import pytest, requests
+import pytest, requests, copy
 
 
 from ..utils.base_url import dev_url_base
 from .payloads.createRegisterPayload import payload_to_create_register
 
 BASE_URL = f"{dev_url_base}"
+CPF_INVALID = "01625708051"
 
 
 def test_create_register_with_success():
     resource = "/create-register"
     url = BASE_URL + resource
-    body = payload_to_create_register
+    body = copy.deepcopy(payload_to_create_register)
     response = requests.post(
         url,
         json=body
@@ -19,3 +20,18 @@ def test_create_register_with_success():
     data = response.json()
     assert response.status_code == 201
     assert data["message"] == message_that_should_come_in_the_response
+
+
+def test_create_register_cpf_invalid():
+    resource = "/create-register"
+    url = BASE_URL + resource
+    body = copy.deepcopy(payload_to_create_register)
+    body["cpf"] = CPF_INVALID
+    response = requests.post(
+        url,
+        json=body
+    )
+    message_that_should_come_in_the_response = "Invalid CPF"
+    data = response.json()
+    assert response.status_code == 400
+    assert data["detail"] == message_that_should_come_in_the_response
--- tests/api/test_deleteRegister.py
+++ tests/api/test_deleteRegister.py
@@ -1,7 +1,6 @@
 import pytest, requests
 from pytest import fixture
 
-
 from ..utils.base_url import dev_url_base
 from .payloads.createRegisterPayload import payload_to_create_register
 
@@ -24,10 +23,8 @@ def create_register_to_delete():
 def test_create_register_with_success(create_register_to_delete):
     resource = "/delete-register/"
     id_to_delete = create_register_to_delete
-    print(id_to_delete)
     id_register_to_delete = str(id_to_delete)
     url = BASE_URL + resource + id_register_to_delete
-    print(url)
 
     response = requests.delete(
         url
