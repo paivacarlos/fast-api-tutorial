45f653f61c4ccd38e16b743e914d946ddb4a5875
Carlos Paiva <paivakk22@gmail.com>
2024-05-18 18:29:08 -0300

    created the function validate_cpf

--- services/createRegisterService.py
+++ services/createRegisterService.py
@@ -1,10 +1,18 @@
 import re
+from fastapi import HTTPException
 from database.connection import connect_to_postgres
 from database.register.createRegisterQuery import create_register_query
 from .utils.formatterPhoneUtils import formatter_Phone
+from .utils.cpfValidatorUtils import validate_cpf
 
 
 async def create_register(new_register):
+    if validate_cpf(new_register.cpf) is not True:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid CPF"
+        )
+
     new_register.phone = await formatter_Phone(new_register.phone)
     # Conexão ao banco de dados PostgreSQL
     conn = await connect_to_postgres()
