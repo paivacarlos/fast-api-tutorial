8092f1638e5cb79f30c221ae32f1922ea29458c5
Carlos Paiva <paivakk22@gmail.com>
2024-05-18 16:36:45 -0300

    bug-fix fixed and the rout createRegister is retourning id

--- database/register/createRegisterQuery.py
+++ database/register/createRegisterQuery.py
@@ -2,11 +2,12 @@ from ..connection import connect_to_postgres
 
 
 async def create_register_query(conn, new_register):
-    await conn.execute(
+    row = await conn.fetchrow(
         "INSERT INTO register (name, cpf, address, phone) "
-        "VALUES ($1, $2, $3, $4)",
+        "VALUES ($1, $2, $3, $4) RETURNING id",
         new_register.name,
         new_register.cpf,
         new_register.address,
         new_register.phone
     )
+    return row['id']
