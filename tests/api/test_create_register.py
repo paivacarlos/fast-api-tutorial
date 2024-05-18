e6594e14304415c199439de99b4b5482947bc132
Carlos Paiva <paivakk22@gmail.com>
2024-05-18 15:47:40 -0300

    added test automation in the route createRegister

---
+++ tests/api/test_create_register.py
@@ -0,0 +1,21 @@
+import pytest, requests
+
+
+from ..utils.base_url import dev_url_base
+from .payloads.createRegisterPayload import payload_to_create_register
+
+BASE_URL = f"{dev_url_base}"
+
+
+def test_create_register_with_success():
+    resource = "/create-register"
+    url = BASE_URL + resource
+    body = payload_to_create_register
+    response = requests.post(
+        url,
+        json=body
+    )
+    should_message_to_response = "Register created with success!"
+    data = response.json()
+    assert response.status_code == 201
+    assert data["message"] == should_message_to_response
