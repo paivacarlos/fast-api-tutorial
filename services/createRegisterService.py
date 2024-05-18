61e35a368dd9afbf4d3b16ab14ffcf169e449ca7
Carlos Paiva <paivakk22@gmail.com>
2024-05-17 21:19:12 -0300

    abstract connection db in the route createRegister

--- services/createRegisterService.py
+++ services/createRegisterService.py
@@ -2,14 +2,7 @@ import re
 import asyncpg
 from dotenv import load_dotenv
 import os
-
-
-load_dotenv()
-database_url = os.getenv("DATABASE_URL")
-
-
-async def connect_to_postgres():
-    return await asyncpg.connect(f"{database_url}")
+from database.connection import connect_to_postgres
 
 
 async def create_register(new_register):
