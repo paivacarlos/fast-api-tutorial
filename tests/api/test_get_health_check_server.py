8b9e351859a79023870eac44a649dad5af73fd78
Carlos Paiva <paivakk22@gmail.com>
2024-05-18 15:54:31 -0300

    adusted assert message

--- tests/api/test_get_health_check_server.py
+++ tests/api/test_get_health_check_server.py
@@ -10,5 +10,8 @@ def test_health_check_server_is_online():
     resource = "/health-check"
     response = requests.get(BASE_URL + resource)
 
-    print(response)
-    assert True
+    data = response.json()
+    messagem_that_should_come_in_the_response = "We are online! ;)"
+
+    assert response.status_code == 200
+    assert data["message"] == messagem_that_should_come_in_the_response
